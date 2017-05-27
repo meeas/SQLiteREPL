02a237ad73edd4554ae2337eb792ebd7a69ab429
nl253 <nl253@kent.ac.uk>
2017-05-27 17:05:36 +0100

    Update.

--- src/db_scripts/utils/scraping.py
+++ src/db_scripts/utils/scraping.py
@@ -3,20 +3,16 @@
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import typing
-from typing import Tuple
+from typing import Tuple, List
 from textblob import TextBlob
 
 Entry = Tuple[str, float, float, str]
 
 class Spider():
-    def __init__(self, max_links: int, theme: str):
-        __start = starting_point
-        __max = max_links
-        __rows = []
-        __theme = theme
-        while len(__rows) > __max:
-            scrape()
+    def __init__(self, max_entries: int, theme: str):
+        self._max = max_entries
+        self._rows = []
+        self._theme = theme
 
 
     def scrape(self, starting_point: str):
@@ -26,17 +22,26 @@ class Spider():
         text = soup.get_text()
         blob = TextBlob(text)
         for sent in blob.sentences:
-            if self.__theme in sent:
+            if self._theme in sent:
                 self._add_entry(str(sent), sent.polarity, sent.subjectivity, focus)
 
-        if text.count(self.__theme) >= 3 and len(self.__rows) > self.__max:
+        if text.count(self._theme) >= 3 and len(self._rows) < self._max:
             anchors = soup.find_all('a', limit=5)
             links = [anchor['href'] for anchor in anchors]
             for url in links:
-                scrape(url)
+                try:
+                    self.scrape(url)
+                except ValueError:
+                    pass
 
+
+    @property
     def results(self) -> List[Entry]:
-        return self.__rows
+        return self._rows
 
-    def _add_entry(self, sent: str, polarity: float, subjectivity: float, URL: str) -> Entry:
-        __rows.append(tuple(sent, polarity, subjectivity, URL))
+
+    def _add_entry(self, sent: str, polarity: float, subjectivity: float, URL: str):
+        self._rows.append(tuple([sent, polarity, subjectivity, URL]))
+
+spider = Spider(30, 'trump')
+spider.scrape('https://www.theguardian.com/us-news/2017/may/27/donald-trumps-europe-tour-leaves-leaders-shaken')
