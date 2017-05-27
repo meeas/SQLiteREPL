f3ac8dd9ffae148486215320151583a5e7d6b1a2
nl253 <nl253@kent.ac.uk>
2017-05-27 18:43:28 +0100

    Push in a sec.

---
+++ src/db_scripts/theme_scraper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from db import create_table
+from utils.scraping import Spider
+
+db_path = "~/.sqlite"
+
+cols = ["sent", "polarity", "subjectivity", "url"]
+
+starting_point = 'https://en.wikipedia.org/wiki/Donald_Trump'
+
+spider = Spider(200, 'Trump')
+
+rows = spider.scrape(starting_point)
+
+rows = spider.results
+
+create_table(rows, 'trump', db_path, cols, delete_existing=True)
--- src/db_scripts/utils/scraping.py
+++ src/db_scripts/utils/scraping.py
@@ -5,15 +5,41 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from typing import Tuple, List
 from textblob import TextBlob
+from re import compile
 
 Entry = Tuple[str, float, float, str]
+
+punctuation = "£%$![]{}~#-+=>^&*`¬</"
+
+class Sanitizer():
+    def __init__(self, text: str):
+        self._text = text
+
+    def _remove_punct(self):
+        translator = str.maketrans(self._text, self._text, "£%$!()[]{}~#-+=>^&*`¬</")
+        self._text.translate(translator)
+
+    def _remove_references(self):
+        self._text = compile("\[\d+\]+").sub("", self._text)
+
+    def _beautify(self):
+        self._text.replace("\n", " ")
+        self._text.replace("\t", " ")
+        self._text = compile(" {2,}").sub(" ", self._text)
+
+    def sanitize(self):
+        self._beautify()
+        self._remove_punct()
+        self._remove_references()
+        return self._text
+
 
 class Spider():
     def __init__(self, max_entries: int, theme: str):
         self._max = max_entries
         self._rows = []
         self._theme = theme
-
+        self._traversed = []
 
     def scrape(self, starting_point: str):
         focus = starting_point
@@ -21,27 +47,36 @@ class Spider():
         soup = BeautifulSoup(html, 'html.parser')
         text = soup.get_text()
         blob = TextBlob(text)
-        for sent in blob.sentences:
-            if self._theme in sent:
-                self._add_entry(str(sent), sent.polarity, sent.subjectivity, focus)
+        sentences = filter(lambda sent: len(sent) < 1500, blob.sentences)
+        for sent in sentences:
 
-        if text.count(self._theme) >= 3 and len(self._rows) < self._max:
-            anchors = soup.find_all('a', limit=5)
-            links = [anchor['href'] for anchor in anchors]
-            for url in links:
+            if self._theme.lower() in sent.lower():
+
+                self._add_entry(Sanitizer(sent.string).sanitize(), sent.polarity,
+                                sent.subjectivity, focus)
+
+        if text.count(self._theme) >= 7 and len(self._rows) < self._max:
+            anchors = soup.find_all('a')
+            links = []
+            for i in range(len(anchors)):
                 try:
-                    self.scrape(url)
+                    links.append(anchors[i]['href'])
+                    # links = [anchor['href'] for anchor in anchors]
+                except KeyError:
+                    pass
+            links = filter(compile('^(https?)').search, links)
+            links = filter(lambda link: link not in self._traversed, links)
+            for url in links:
+                self._traversed.append(url)
+                try:
+                    return self.scrape(url)
                 except ValueError:
                     pass
-
+        return
 
     @property
     def results(self) -> List[Entry]:
         return self._rows
 
-
     def _add_entry(self, sent: str, polarity: float, subjectivity: float, URL: str):
         self._rows.append(tuple([sent, polarity, subjectivity, URL]))
-
-spider = Spider(30, 'trump')
-spider.scrape('https://www.theguardian.com/us-news/2017/may/27/donald-trumps-europe-tour-leaves-leaders-shaken')
