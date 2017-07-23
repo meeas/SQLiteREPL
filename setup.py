c97e8afcb0122b1752f3b1b4339d89745cc3677c
nl253 <nl253@kent.ac.uk>
2017-07-23 19:04:05 +0100

    Update.

--- setup.py
+++ setup.py
@@ -17,53 +17,52 @@ here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-
     long_description = f.read()
 
-    setup(
-        name='sqlite',
+setup(
 
-        # Versions should comply with PEP440.  For a discussion on single-sourcing
-        # the version across setup.py and the project code, see
-        # https://packaging.python.org/en/latest/single_source_version.html
-        version='0.2.0',
+    name='sqlite',
 
-        description='SQLite Clinet written in python3',
-        long_description=long_description,
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.2.0',
 
-        # The project's main homepage.
-        url='https://github.com/nl253/SQLiteREPL',
+    description='SQLite Clinet written in python3',
 
-        # Author details
-        author='Norbert Logiewa',
+    long_description=long_description,
 
-        author_email='nl253@kent.ac.uk',
+    # The project's main homepage.
+    url='https://github.com/nl253/SQLiteREPL',
 
-        # Choose your license
-        license='MIT',
+    # Author details
+    author='Norbert Logiewa',
 
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Topic :: Database :: Front-Ends',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-        ],
+    author_email='norbertlogiewa96\@gmail.com',
 
-        keywords='database sqllite db-clinet SQLite prompt-toolkit',
+    # Choose your license
+    license='MIT',
 
-        # You can just specify the packages manually here if your project is
-        # simple. Or you can use find_packages().
-        packages=find_packages(exclude=['contrib', 'docs', 'tests', 'screens']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Database :: Front-Ends',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 
-        # List run-time dependencies here.  These will be installed by pip when
-        # your project is installed. For an analysis of "install_requires" vs pip's
-        # requirements files see:
-        # https://packaging.python.org/en/latest/requirements.html
+    keywords='database sqllite3 sqlite clinet SQLite prompt-toolkit prompt_toolkit',
 
-        install_requires=['pandas', 'prompt_toolkit'],
+    packages=find_packages(),
 
-        entry_points={
-            'console_scripts': ['sqlite = main:cli']
-            })
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+
+    install_requires=['pandas', 'prompt_toolkit'],
+
+    entry_points={
+        'console_scripts': ['sqlite = main:cli']
+        })
